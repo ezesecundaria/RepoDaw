30f18f5ae7ad65355bf432436bed1b23280029ae
Alumno <Alumno@192.168.0.170>
2017-03-09 11:58:52 +0100

    Subida del primer programa 'hola mundo'

---
+++ App Hola desde local/src/app/hola/desde/local/AppHolaDesdeLocal.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app.hola.desde.local;
+
+/**
+ *
+ * @author Alumno
+ */
+public class AppHolaDesdeLocal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Esto lo he hecho desde el ordenador local.");
+        System.out.print("Una vez que hago las modificaciones, tendré que hacer un commit para almacenarlo"
+                + "localmente y luego un push para subirlo al github...");
+    }
+    
+}
